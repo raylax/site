e4e279ea8c9ee346ff69ef2c4f471842259470c5
raylax <raylax@inurl.org>
2023-09-12 16:46:32 +0800

    deploy

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = '_'
-export const SITE_DESCRIPTION = '___'
+export const SITE_TITLE = 'RAYLAX'
+export const SITE_DESCRIPTION = 'RAYLAX site'
 
 export const PAGE_SIZE = 12
